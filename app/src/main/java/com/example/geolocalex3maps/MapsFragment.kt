f501d4db8550ccd0ee165409bac92b1104ab9e1a
Student04 <student04@TFNS5304.techni.dom>
2021-09-02 11:04:21 +0200

    implementation Maps

---
+++ app/src/main/java/com/example/geolocalex3maps/MapsFragment.kt
@@ -0,0 +1,74 @@
+package com.example.geolocalex3maps
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment() {
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 123
+    }
+
+    private lateinit var map: GoogleMap
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
+        tracklocation()
+    }
+
+    private fun tracklocation(){
+        if (ActivityCompat.checkSelfPermission (requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ), LOCATION_PERMISSION_REQUEST_CODE
+            )
+
+            return
+        }
+        map.isMyLocationEnabled = true
+    }
+    fun  onPermissionGranted(){
+        tracklocation()
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
